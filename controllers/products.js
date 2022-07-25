3add137014db85b135c6ba8edc6e5fa5df9c5699
waelzegneni <waelzegneni@gmail.com>
2022-07-25 22:37:02 +0100

    products

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,22 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query)
+  const { featured, company, name } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = name
+  }
+
+  console.log(queryObject)
+
+  const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length })
 }
 
